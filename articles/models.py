e51889e0ff9a39c63bfee257da487cf40918ca53
Mark Gituma <gitumarkk@gmail.com>
2013-06-27 15:11:06 +0200

    Default for publish_at is .now instead of .now()
    Date time field is now updated for each new record

--- articles/models.py
+++ articles/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class Article(models.Model):
     article = models.CharField(max_length=480, blank=False)
     publish = models.BooleanField()
-    publish_at = models.DateTimeField(blank=False, default=timezone.now())
+    publish_at = models.DateTimeField(default=timezone.now, blank=False)
     created_at = models.DateTimeField(auto_now_add=True, blank=False, editable=False)
 
     def __unicode__(self):
