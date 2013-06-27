de3470393b8d8e883e5e021c42b274451ddf36fe
Mark Gituma <gitumarkk@gmail.com>
2013-06-27 14:01:48 +0200

    Changed Article model field names

--- articles/models.py
+++ articles/models.py
@@ -5,8 +5,8 @@ from django.utils import timezone
 class Article(models.Model):
     article = models.CharField(max_length=480, blank=False)
     publish = models.BooleanField()
-    date_published = models.DateTimeField(blank=False, default=timezone.now())
-    created = models.DateTimeField(auto_now_add=True, blank=False, editable=False)
+    publish_at = models.DateTimeField(blank=False, default=timezone.now())
+    created_at = models.DateTimeField(auto_now_add=True, blank=False, editable=False)
 
     def __unicode__(self):
         return self.article
