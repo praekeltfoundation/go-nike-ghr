fdfc0de5638b9927af2feed1bd85cdfaf3432540
Mark Gituma <gitumarkk@gmail.com>
2013-06-27 19:10:02 +0200

    Created view for ques and answers on same page
    Mike, please have a look at this solution on the admin and see if I am
    on the right ladder (I am actually impressed at Django for its
    capabilities).

--- articles/admin.py
+++ articles/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from articles.models import Article, MonitorAndLearningQuizId, MonitorAndLearningQuizQuestion, MonitorAndLearningQuizAnswer
 
 
+class MonitorAndLearningQuizAnswerInline(admin.StackedInline):
+    model = MonitorAndLearningQuizAnswer
+    extra = 3
+    max_num = 3
+
+
 class ArticleAdmin(admin.ModelAdmin):
     pass
 
@@ -11,14 +17,8 @@ class MonitorAndLearningQuizIdAdmin(admin.ModelAdmin):
 
 
 class MonitorAndLearningQuizQuestionAdmin(admin.ModelAdmin):
-    pass
-
-
-class MonitorAndLearningQuizAnswerAdmin(admin.ModelAdmin):
-    pass
-
+    inlines = [MonitorAndLearningQuizAnswerInline]
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(MonitorAndLearningQuizId, MonitorAndLearningQuizIdAdmin)
 admin.site.register(MonitorAndLearningQuizQuestion, MonitorAndLearningQuizQuestionAdmin)
-admin.site.register(MonitorAndLearningQuizAnswer, MonitorAndLearningQuizAnswerAdmin)
