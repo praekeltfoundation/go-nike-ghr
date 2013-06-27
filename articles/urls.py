2eae69268a5231c7588ba21b558d259705a85abd
Mark Gituma <gitumarkk@gmail.com>
2013-06-27 16:43:32 +0200

    Changed the urls to r'^articles from r'^articles/

--- articles/urls.py
+++ articles/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
-    url(r'^articles/$', 'articles.views.get_articles'),
+    url(r'^articles$', 'articles.views.get_articles'),
 )
