2c1cf9d98a91565198289f19cc2dc161c4c99200
Mark Gituma <gitumarkk@gmail.com>
2013-06-27 17:38:47 +0200

    Registered M&L with admin

--- articles/admin.py
+++ articles/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from articles.models import Article
+from articles.models import Article, MonitorQuizId, MonitorQuizQuestion, MonitorQuizAnswer
 
 admin.site.register(Article)
+admin.site.register(MonitorQuizId)
+admin.site.register(MonitorQuizQuestion)
+admin.site.register(MonitorQuizAnswer)
