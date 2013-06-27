6b3298e222e372bf4528088c675872987c7d5522
Mark Gituma <gitumarkk@gmail.com>
2013-06-27 16:05:12 +0200

    Added URL to find required API function in views

--- ghr/urls.py
+++ ghr/urls.py
@@ -11,8 +11,10 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^celery/', include('celery_app.urls')),
+
+    # API links
+    url(r'^api/', include('articles.urls')),
 )
