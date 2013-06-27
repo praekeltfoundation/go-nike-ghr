b567074161c891333a0692fda98b69d82d57eb3b
Mark Gituma <gitumarkk@gmail.com>
2013-06-28 01:35:44 +0200

    Char length check < 160 working (BOOM)

--- articles/admin.py
+++ articles/admin.py
@@ -1,12 +1,33 @@
 from django.contrib import admin
 from articles.models import Article, MonitorAndLearningQuizId, MonitorAndLearningQuizQuestion, MonitorAndLearningQuizAnswer
 from django import forms
+from django.forms.models import BaseInlineFormSet
+
+
+class MonitorAndLearningQuiz(BaseInlineFormSet):
+    # Validating the formset data
+
+    def clean(self):
+        super(MonitorAndLearningQuiz, self).clean()
+
+        char_limit = len(self.instance.question)
+        for form in self.forms:
+            if not hasattr(form, 'cleaned_data'):
+                continue
+            form.cleaned_data['answer']
+            char_limit = char_limit + len(form.cleaned_data['answer'])
+
+            if char_limit > 160:
+                raise forms.ValidationError("You have gone beyond the character limit"
+                                            " please shorten questions and/or answers")
+
 
 
 class MonitorAndLearningQuizAnswerInline(admin.StackedInline):
     model = MonitorAndLearningQuizAnswer
     extra = 3
     max_num = 3
+    formset = MonitorAndLearningQuiz
 
 
 class ArticleAdmin(admin.ModelAdmin):
@@ -19,16 +40,6 @@ class MonitorAndLearningQuizIdAdmin(admin.ModelAdmin):
 
 class MonitorAndLearningQuizQuestionAdmin(admin.ModelAdmin):
     inlines = [MonitorAndLearningQuizAnswerInline]
-
-    def save_formset(self, request, form, formset, change):
-        char_limit = len(form.instance.question)
-        for form_item in formset:
-            char_limit = char_limit + len(form_item.cleaned_data['answer'])
-            if char_limit > 160:
-                raise forms.ValidationError("You have gone beyond the character limit"
-                                            " please shorten questions and/or answers")
-        formset.save()
-        form.instance.save()
 
 
 admin.site.register(Article, ArticleAdmin)
