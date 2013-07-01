094e4222643ecd25b8cf4df912183053c7368ef9
Mike Jones <mike@westerncapelabs.com>
2013-07-01 15:43:40 +0200

    Added real sector validation

--- js_sandbox/lib/go-nike-ghr.js
+++ js_sandbox/lib/go-nike-ghr.js
@@ -49,6 +49,14 @@ function GoNikeGHR() {
         return p;
     };
 
+    self.validate_sector = function(im, sector){
+        if (im.config.sectors.indexOf(sector.toLowerCase())!=-1){
+            return true;
+        } else {
+            return false;
+        }
+    };
+
     self.add_creator('initial_state', function(state_name, im) {
         // Check if they've already registered
         var p = self.get_contact(im);
@@ -131,7 +139,7 @@ function GoNikeGHR() {
 
     self.add_creator('reg_thanks', function(state_name, im) {
         var sector = im.get_user_answer('reg_sector');
-        if (sector=='Valid sector') {
+        if (self.validate_sector(im, sector)) {
             // Get the user
             var p = self.get_contact(im);
 
