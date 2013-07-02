addb45d837ecdb74e72cfc145514f04e34d12c4a
Mark Gituma <gitumarkk@gmail.com>
2013-07-02 15:12:53 +0200

    Creating Tests for M&L API (TDD)

---
+++ monitor_and_learn/tests/test_api.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+import json
+
+
+class TestMLAPI(TestCase):
+    fixtures = ['test/test_M_and_L.json']
+
+    def test_correct_url(self):
+        response = self.client.get('/api/mandl/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_correct_data(self):
+        url = reverse('api_dispatch_list',
+                      kwargs={'resource_name': 'mandl',
+                      'api_name': 'api'})
+
+        response = self.client.get(url)
+        self.assertEqual("application/json", response["Content-Type"])
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.content)["quizzes"], [1, 2, 3])
