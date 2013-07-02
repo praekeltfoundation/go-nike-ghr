6306a9aeeb74f9b3eec6a446dfad2aae5f8e284f
Mark Gituma <gitumarkk@gmail.com>
2013-07-02 17:21:34 +0200

    Creating a TextArea widget for the page_# field

--- articles/admin.py
+++ articles/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from articles.models import Article
+from django import forms
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        # Overiding the page CharField widget to Text Area
+        formfield = super(ArticleAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+        if db_field.name == 'page_1':
+            formfield.widget = forms.Textarea(attrs=formfield.widget.attrs)
+        if db_field.name == 'page_2':
+            formfield.widget = forms.Textarea(attrs=formfield.widget.attrs)
+        if db_field.name == 'page_3':
+            formfield.widget = forms.Textarea(attrs=formfield.widget.attrs)
+        if db_field.name == 'page_4':
+            formfield.widget = forms.Textarea(attrs=formfield.widget.attrs)
+        return formfield
 
 # Registering the Article with the admin
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
