cff34f3f2e2581fb662fc5f57821c7b9cca6a478
Mark Gituma <gitumarkk@gmail.com>
2013-07-02 16:55:18 +0200

    Updated unittests for changes
    Changed unittests to take individual article pages as opposed to one
    article

--- articles/tests/test_views.py
+++ articles/tests/test_views.py
@@ -5,18 +5,19 @@ import json
 
 
 class TestArticlesAPIWorks(TestCase):
-    fixtures = ['test/articles/articles_api_good_test_data.json']
+    fixtures = ['test/articles_api_good_test_data.json']
 
     def test_api_works(self):
-        # Tests to see if API is functioning correctly (however since it is loading
-        #  json objects the tests will fail in a week from 29-June)
+        # Tests to see if API is functioning correctly (however since it
+        # is loading json objects the tests will fail in a week from 29-June)
         url = reverse('api_dispatch_list',
                       kwargs={'resource_name': 'article',
                       'api_name': 'api'})
         response = self.client.get(url)
         self.assertEqual("application/json", response["Content-Type"])
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(json.loads(response.content)['article'], 'Test1')
+        self.assertEqual(json.loads(response.content)['article'][0],
+                         unicode('Test 1'))
 
     def test_correct_url(self):
         # Tests teh URL path to see if it is what its expected
@@ -25,14 +26,14 @@ class TestArticlesAPIWorks(TestCase):
 
 
 class TestAPIArticlesNoNewArticle(TestCase):
-    fixtures = ['test/articles/articles_api_unpublished_test_data.json']
+    fixtures = ['test/articles_api_non_lastweek_test_data.json']
 
     def test_no_article_last_week(self):
         # Tests response for no articles published in the last 7 days.
         get_fixture = Article.objects.get(pk=1)
 
         # checking if fixture loads
-        self.assertEqual(get_fixture.article, "Test1")
+        self.assertEqual(get_fixture.page_1, unicode("Test 1"))
 
         # Testing the API
         url = reverse('api_dispatch_list',
@@ -41,20 +42,21 @@ class TestAPIArticlesNoNewArticle(TestCase):
 
         response = self.client.get(url)
         self.assertEqual("application/json", response["Content-Type"])
+
         self.assertEqual(json.loads(response.content)['article'],
                          "Sorry there's no article this week, dial back soon!")
         self.assertEqual(response.status_code, 200)
 
 
 class TestAPIArticlesNotPublished(TestCase):
-    fixtures = ['test/articles/articles_api_non_lastweek_test_data.json']
+    fixtures = ['test/articles_api_unpublished_test_data.json']
 
     def test_article_not_published(self):
         # Tests to see if articles are un-published.
         get_fixture = Article.objects.get(pk=1)
 
         # checking if fixture loads
-        self.assertEqual(get_fixture.article, "Test1")
+        self.assertEqual(get_fixture.page_1, unicode("Test 1"))
 
         # Testing the API
         url = reverse('api_dispatch_list',
