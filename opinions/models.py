d34f9ba831023b84fc5f84bea119da8c0098a8cc
Mark Gituma <gitumarkk@gmail.com>
2013-07-03 23:18:18 +0200

    Changed Model name to Opinion and added Meta info

--- opinions/models.py
+++ opinions/models.py
@@ -1,17 +1,19 @@
 from django.db import models
-from django.utils import timezone
 
 
-class Article(models.Model):
+class Opinion(models.Model):
     # This class is for the Article model.
     opinion_1 = models.CharField(max_length=125)
     opinion_2 = models.CharField(max_length=125)
     opinion_3 = models.CharField(max_length=125)
     opinion_4 = models.CharField(max_length=125)
     opinion_5 = models.CharField(max_length=125)
-    updated_at = models.DateTimeField(default=timezone.now,
+    updated_at = models.DateTimeField(auto_now_add=True,
                                       blank=False,
                                       editable=False)
 
     def __unicode__(self):
         return self.opinion_1
+
+    class Meta:
+        verbose_name_plural= "User Opinions"
