4e2d4b1cd5edf1c92498ca35f981fcdca215b0f9
Mark Gituma <gitumarkk@gmail.com>
2013-07-03 12:30:51 +0200

    Made changes to admin Textarea Widget

--- articles/admin.py
+++ articles/admin.py
@@ -7,13 +7,8 @@ class ArticleAdmin(admin.ModelAdmin):
     def formfield_for_dbfield(self, db_field, **kwargs):
         # Overiding the page CharField widget to Text Area
         formfield = super(ArticleAdmin, self).formfield_for_dbfield(db_field, **kwargs)
-        if db_field.name == 'page_1':
-            formfield.widget = forms.Textarea(attrs=formfield.widget.attrs)
-        if db_field.name == 'page_2':
-            formfield.widget = forms.Textarea(attrs=formfield.widget.attrs)
-        if db_field.name == 'page_3':
-            formfield.widget = forms.Textarea(attrs=formfield.widget.attrs)
-        if db_field.name == 'page_4':
+
+        if db_field.name in['page_1', 'page_2', 'page_3', 'page_4']:
             formfield.widget = forms.Textarea(attrs=formfield.widget.attrs)
         return formfield
 
