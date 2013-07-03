eaa5f684379f081d975f0effe710fcffcf8457bc
Mark Gituma <gitumarkk@gmail.com>
2013-07-03 17:46:15 +0200

    Fixing bug (initially assumed objects are sorted)
    Fixed a bug where initially I assumed all objects are sorted from the
    database (was testing with sqlite3), when tested with postgresql, the
    query was sorted differently hence made a fix that assumes that data is
    unsorted.

--- monitor_and_learn/api.py
+++ monitor_and_learn/api.py
@@ -34,26 +34,33 @@ class MonitorAndLearningQuizIDResource(ModelResource):
 
     def alter_detail_data_to_serialize(self, request, data_dict):
         # Altering the response given by accessing different quiz IDs
-        dict_item = {}
+        dict_item = {}  # Dict item to hold final structure
         quiz_ids = data_dict.data["quiz_ids"]
+        min_id = []  # Variable to hold min id for start key
+        max_id = []  # Variable to hold the max question id for menu endpoint
 
+        for i in range(len(quiz_ids)):
+            max_id.append(quiz_ids[i].data["id"])
+
+        max_id = max(max_id)
         for i in range(len(quiz_ids)):
             q_id = "q_%s" % quiz_ids[i].data["id"]
             dict_item[q_id] = {"choices": []}
             x = []
+            min_id.append(quiz_ids[i].data["id"])
 
             for j in range(len(quiz_ids[i].data["quiz_ids"])):
-                if (i) >= (len(quiz_ids) - 1):
+                if quiz_ids[i].data["id"] == max_id:
                     y = "main_menu"
                 else:
-                    y = "q_%s" % (i + 2)
+                    y = "q_%s" % (quiz_ids[i].data["id"] + 1)
                 z = quiz_ids[i].data["quiz_ids"][j].data["answer"]
                 x.append([y, z])
 
             dict_item[q_id] = {"question": quiz_ids[i].data["question"],
                                "choices": x}
-
-        data_dict.data["quiz"] = {"start": "q_%s" % quiz_ids[0].data["id"],
+        min_id = min(min_id)
+        data_dict.data["quiz"] = {"start": "q_%s" % min_id,
                                   "questions": dict_item}
         del data_dict.data["quiz_ids"]
         del data_dict.data["id"]
--- monitor_and_learn/tests/test_api.py
+++ monitor_and_learn/tests/test_api.py
@@ -40,11 +40,11 @@ class TestMLAPI(TestCase):
         self.assertEqual(json_item["quiz"]["questions"]["q_4"]["question"], "Q1D")
 
         # Asserting the answers
-        self.assertEqual(json_item["quiz"]["questions"]["q_1"]["choices"],
-                         [["q_2", "A1Aa"], ["q_2", "A1Ab"], ["q_2", "A1Ac"]])
-        self.assertEqual(json_item["quiz"]["questions"]["q_2"]["choices"],
-                         [["q_3", "A1Ba"], ["q_3", "A1Bb"], ["q_3", "A1Bc"]])
-        self.assertEqual(json_item["quiz"]["questions"]["q_3"]["choices"],
-                         [["q_4", "A1Ca"], ["q_4", "A1Cb"], ["q_4", "A1Cc"]])
-        self.assertEqual(json_item["quiz"]["questions"]["q_4"]["choices"],
-                         [["main_menu", "A1Da"], ["main_menu", "A1Db"], ["main_menu", "A1Dc"]])
+        self.assertEqual(sorted(json_item["quiz"]["questions"]["q_1"]["choices"]),
+                         sorted([["q_2", "A1Aa"], ["q_2", "A1Ab"], ["q_2", "A1Ac"]]))
+        self.assertEqual(sorted(json_item["quiz"]["questions"]["q_2"]["choices"]),
+                         sorted([["q_3", "A1Ba"], ["q_3", "A1Bb"], ["q_3", "A1Bc"]]))
+        self.assertEqual(sorted(json_item["quiz"]["questions"]["q_3"]["choices"]),
+                         sorted([["q_4", "A1Ca"], ["q_4", "A1Cb"], ["q_4", "A1Cc"]]))
+        self.assertEqual(sorted(json_item["quiz"]["questions"]["q_4"]["choices"]),
+                         sorted([["main_menu", "A1Da"], ["main_menu", "A1Db"], ["main_menu", "A1Dc"]]))
