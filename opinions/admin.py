9d721d1f336732344cee708d89cf0ee7e0525722
Mark Gituma <gitumarkk@gmail.com>
2013-07-03 23:24:24 +0200

    Adding Textareawidget to Admin interface

--- opinions/admin.py
+++ opinions/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from opinions.models import Opinion
+from django import forms
 
 
 class OpinionAdmin(admin.ModelAdmin):
-    pass
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        # Overiding the opinion CharField widget to Text Area
+        formfield = super(OpinionAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+
+        if db_field.name in ['opinion_1', 'opinion_2', 'opinion_3',
+                             'opinion_4', 'opinion_5']:
+            formfield.widget = forms.Textarea(attrs=formfield.widget.attrs)
+        return formfield
 
 # Registering the Article with the admin
 admin.site.register(Opinion, OpinionAdmin)
