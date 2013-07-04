942bf36fa5bbf9dd6b8495775cf24a00efdb409c
Mark Gituma <gitumarkk@gmail.com>
2013-07-04 22:42:24 +0200

    Created the required model

--- weekly_quiz/models.py
+++ weekly_quiz/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class WeeklyQuizId(models.Model):
+    # The quiz class which forms the Foreign Key for the
+    # questions, contains an active field
+    # where the quize can be set to active and a completed
+    # field where if 4 questions are it changes to complete = True
+    name = models.CharField(max_length=50, blank=False,
+                            verbose_name="Name of Quiz")
+    active = models.BooleanField()
+    completed = models.BooleanField()
+
+    def __str__(self):
+        # Provides a User friendly name
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Weekly Quiz Status"
+
+
+class WeeklyQuizQuestion(models.Model):
+    # This class stores the questions to be asked
+    quiz_id = models.ForeignKey('WeeklyQuizId',
+                                related_name='weekly_quiz_question',
+                                verbose_name='Weekly Quiz Name')
+    question = models.CharField(max_length=160, blank=False)
+
+    def __unicode__(self):
+        return self.question
+
+    class Meta:
+        verbose_name = "Weekly Quiz Question"
+
+
+class WeeklyQuizAnswer(models.Model):
+    # This class stores the answers.
+    question_id = models.ForeignKey('WeeklyQuizQuestion',
+                                    related_name="weekly_quiz_answer")
+    answer = models.CharField(max_length=160)
+
+    def __unicode__(self):
+        return self.answer
