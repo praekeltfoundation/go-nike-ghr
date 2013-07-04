b3b717aa87d1ea24ab4a7b19358b685a86bb946c
Mark Gituma <gitumarkk@gmail.com>
2013-07-04 19:28:12 +0200

    Added validation on # of questions & answers

--- monitor_and_learn/admin.py
+++ monitor_and_learn/admin.py
@@ -9,17 +9,17 @@ from django.forms.models import BaseInlineFormSet
 class QuestionAdminForm(forms.ModelForm):
     # This form checks to see if there are any uncompleted quizzes
     #  (< 4 questions), if so sets current ID to the uncompleted quiz_id.
-    # If editing sets the current quiz id  to be edited.
+    # If editing sets the current quiz id  to be edited
     def __init__(self, *args, **kwargs):
         super(QuestionAdminForm, self).__init__(*args, **kwargs)
         choices = [self.fields["quiz_id"].choices.__iter__().next()]
         choices.pop()
-        query = MonitorAndLearningQuizId.objects.all().filter(completed=False).order_by('pk')
+        query = (MonitorAndLearningQuizId.objects.all().
+                 filter(completed=False).order_by('pk'))
 
         if 'question' in self.initial:
             get = query = MonitorAndLearningQuizId.objects.get(pk=
                                                                self.initial['quiz_id'])
-            print get.name
             choices.append((self.initial['quiz_id'], get.name))
         else:
             if query.exists():
@@ -27,6 +27,30 @@ class QuestionAdminForm(forms.ModelForm):
                     choices.append((query[item].pk, query[item].name))
 
         self.fields['quiz_id'].choices = choices
+
+
+class QuizIdAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
+        super(QuizIdAdminForm, self).__init__(*args, **kwargs)
+
+    class Meta:
+        model = MonitorAndLearningQuizId
+
+    def clean(self, *args, **kwargs):
+        #  Checking the active field vs number of questions completed
+        #  to see if quiz should be activated
+        cleaned_data = super(QuizIdAdminForm, self).clean(*args, **kwargs)
+
+        if cleaned_data['active']:
+            query = (MonitorAndLearningQuizQuestion.objects.
+                     all().filter(quiz_id_id=self.instance.id))
+            print query
+            if len(query) != 4:
+                raise forms.ValidationError("This quiz has less than 4 "
+                                            "questions, finish questions to "
+                                            "activate")
+        return cleaned_data
 
 
 class MonitorAndLearningQuizAnswerFormset(BaseInlineFormSet):
@@ -37,22 +61,26 @@ class MonitorAndLearningQuizAnswerFormset(BaseInlineFormSet):
         # also checks if the answer has been submitted and Foreign key has
         # been included
         super(MonitorAndLearningQuizAnswerFormset, self).clean()
-
+        i = 0
         char_limit = len(self.instance.question)
         for form in self.forms:
             if not hasattr(form, 'cleaned_data'):
                 continue
-            if "answer" not in form.cleaned_data:
+
+            if "answer" in form.cleaned_data:
+                i = i + 1
+
+            if ("answer" not in form.cleaned_data and i < 2):
                 raise forms.ValidationError("You need to complete the answers")
+            else:
+                if "answer" in form.cleaned_data:
+                    char_limit = char_limit + len(form.cleaned_data['answer'])
 
-            form.cleaned_data['answer']
-            char_limit = char_limit + len(form.cleaned_data['answer'])
-
-            if char_limit > 160:
-                raise forms.ValidationError("You have gone beyond the"
-                                            " character limit"
-                                            " please shorten questions "
-                                            "and/or answers")
+                    if char_limit > 160:
+                        raise forms.ValidationError("You have gone beyond the"
+                                                    " character limit"
+                                                    " please shorten questions"
+                                                    " and/or answers")
 
         try:
         # Checking if Foreign ID has been included otherwise gives
@@ -71,19 +99,20 @@ class MonitorAndLearningQuizAnswerFormset(BaseInlineFormSet):
 class MonitorAndLearningQuizAnswerInline(admin.StackedInline):
     # This class sets the answers to be on the same page as the parent question
     model = MonitorAndLearningQuizAnswer
-    extra = 3
-    max_num = 3
+    extra = 2
     formset = MonitorAndLearningQuizAnswerFormset
 
 
 class MonitorAndLearningQuizIdAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["name", "active", "completed"]
+    form = QuizIdAdminForm
 
 
 class MonitorAndLearningQuizQuestionAdmin(admin.ModelAdmin):
     # The admin class that adds extra fields to the QuestionAdmin Section
     inlines = [MonitorAndLearningQuizAnswerInline]
     form = QuestionAdminForm
+    list_display = ["question", "quiz_id"]
 
 
 admin.site.register(MonitorAndLearningQuizId, MonitorAndLearningQuizIdAdmin)
