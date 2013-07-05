f7d100a23d70195c1bbbf0a44147de59f9193681
Mark Gituma <gitumarkk@gmail.com>
2013-07-05 15:59:18 +0200

    Created the model structure for the directory

--- directory/models.py
+++ directory/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=60, blank=False,
+                            verbose_name="Category Name")
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Category"
+
+
+class SubCategory(models.Model):
+    # This class stores the questions to be asked
+    category = models.ForeignKey('WeeklyQuizId',
+                                 related_name='category',
+                                 verbose_name='Category Id')
+
+    name = models.CharField(max_length=125)
+    content_1 = models.CharField(max_length=125)
+    content_1 = models.CharField(max_length=125)
+    content_1 = models.CharField(max_length=125)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Sub Category"
