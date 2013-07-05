016879d5431676e4ccbd83e4146573b33cce2ea7
Mark Gituma <gitumarkk@gmail.com>
2013-07-05 02:38:34 +0200

    Made correction to urls

--- weekly_quiz/urls.py
+++ weekly_quiz/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, url, include
-from monitor_and_learn.api import MonitorAndLearningQuizIDResource
+from weekly_quiz.api import WeeklyQuizIDResource
 from tastypie.api import Api
 
 
 # Setting the API base name and registering the API resources using
 # Tastypies API function
 api_resources = Api(api_name='api')
-api_resources.register(MonitorAndLearningQuizIDResource())
+api_resources.register(WeeklyQuizIDResource())
 api_resources.prepend_urls()
 
 # Setting the urlpatterns to hook into the api urls
