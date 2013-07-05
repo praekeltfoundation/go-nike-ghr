7e9216d5a757d4ecca8df2362413c9e415f2c210
Mark Gituma <gitumarkk@gmail.com>
2013-07-05 15:15:54 +0200

    Addes comments to admin and delete print statment

--- monitor_and_learn/admin.py
+++ monitor_and_learn/admin.py
@@ -7,20 +7,22 @@ from django.forms.models import BaseInlineFormSet
 
 
 class QuestionAdminForm(forms.ModelForm):
-    # This form checks to see if there are any uncompleted quizzes
-    #  (< 4 questions), if so sets current ID to the uncompleted quiz_id.
-    # If editing sets the current quiz id  to be edited
+    """
+    This form checks to see if there are any uncompleted quizzes
+    (< 4 questions), if so sets current ID to the uncompleted quiz_id.
+    If editing sets the current quiz id  to be edited
+    """
     def __init__(self, *args, **kwargs):
         super(QuestionAdminForm, self).__init__(*args, **kwargs)
         choices = [self.fields["quiz_id"].choices.__iter__().next()]
-        choices.pop()
+        choices.pop()  # Removing empty choice so it defaults to empty
         query = (MonitorAndLearningQuizId.objects.all().
                  filter(completed=False).order_by('pk'))
 
         if 'question' in self.initial:
-            get = query = MonitorAndLearningQuizId.objects.get(pk=
-                                                               self.initial['quiz_id'])
-            choices.append((self.initial['quiz_id'], get.name))
+            get_quiz = MonitorAndLearningQuizId.objects.get(pk=
+                                                            self.initial['quiz_id'])
+            choices.append((self.initial['quiz_id'], get_quiz.name))
         else:
             if query.exists():
                 for item in range(len(query)):
@@ -30,6 +32,9 @@ class QuestionAdminForm(forms.ModelForm):
 
 
 class QuizIdAdminForm(forms.ModelForm):
+    """
+    This form checks if there are 4 questions in quiz before it can be activated
+    """
     def __init__(self, *args, **kwargs):
         self.request = kwargs.pop('request', None)
         super(QuizIdAdminForm, self).__init__(*args, **kwargs)
@@ -45,7 +50,6 @@ class QuizIdAdminForm(forms.ModelForm):
         if cleaned_data['active']:
             query = (MonitorAndLearningQuizQuestion.objects.
                      all().filter(quiz_id_id=self.instance.id))
-            print query
             if len(query) != 4:
                 raise forms.ValidationError("This quiz has less than 4 "
                                             "questions, finish questions to "
@@ -54,12 +58,20 @@ class QuizIdAdminForm(forms.ModelForm):
 
 
 class MonitorAndLearningQuizAnswerFormset(BaseInlineFormSet):
-    # This class validates the formset model and checks for max_length
+    """
+    This class:
+        - Checks if answers have been included fails if < 2
+        - Checks if responses have been included
+        - Checks the max_length for the questions and answers
+        - If 4 questions have been added toggles to complete
+    """
 
     def clean(self):
-        # Overiding the clean function so that the max length can be chekced
-        # also checks if the answer has been submitted and Foreign key has
-        # been included
+        """
+        Overiding the clean function so that the max length can be checked
+        also checks if the answer has been submitted and Foreign key has
+        been included
+        """
         super(MonitorAndLearningQuizAnswerFormset, self).clean()
         i = 0
         char_limit = len(self.instance.question)
@@ -97,19 +109,26 @@ class MonitorAndLearningQuizAnswerFormset(BaseInlineFormSet):
 
 
 class MonitorAndLearningQuizAnswerInline(admin.StackedInline):
-    # This class sets the answers to be on the same page as the parent question
+    """
+    This class sets the answers to be on the same page as the parent question
+    """
     model = MonitorAndLearningQuizAnswer
     extra = 2
     formset = MonitorAndLearningQuizAnswerFormset
 
 
 class MonitorAndLearningQuizIdAdmin(admin.ModelAdmin):
+    """
+    This class sets handles extra functionality for QuizID Admin section
+    """
     list_display = ["name", "active", "completed"]
     form = QuizIdAdminForm
 
 
 class MonitorAndLearningQuizQuestionAdmin(admin.ModelAdmin):
-    # The admin class that adds extra fields to the QuestionAdmin Section
+    """
+    This class sets handles extra functionality for Question Admin section
+    """
     inlines = [MonitorAndLearningQuizAnswerInline]
     form = QuestionAdminForm
     list_display = ["question", "quiz_id"]
