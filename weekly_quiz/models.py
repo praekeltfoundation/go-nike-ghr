5d2a2687544945196bd5f47501f23a8ada3218d3
Mark Gituma <gitumarkk@gmail.com>
2013-07-05 02:38:00 +0200

    Modified models for related name and did south m

--- weekly_quiz/models.py
+++ weekly_quiz/models.py
@@ -9,7 +9,7 @@ class WeeklyQuizId(models.Model):
     name = models.CharField(max_length=50, blank=False,
                             verbose_name="Name of Quiz")
     active = models.BooleanField()
-    completed = models.BooleanField()
+    completed = models.BooleanField(editable=False)
 
     def __str__(self):
         # Provides a User friendly name
@@ -22,7 +22,7 @@ class WeeklyQuizId(models.Model):
 class WeeklyQuizQuestion(models.Model):
     # This class stores the questions to be asked
     quiz_id = models.ForeignKey('WeeklyQuizId',
-                                related_name='weekly_quiz_question',
+                                related_name='wq_quiz_id',
                                 verbose_name='Weekly Quiz Name')
     question = models.CharField(max_length=160, blank=False)
 
@@ -36,8 +36,9 @@ class WeeklyQuizQuestion(models.Model):
 class WeeklyQuizAnswer(models.Model):
     # This class stores the answers.
     question_id = models.ForeignKey('WeeklyQuizQuestion',
-                                    related_name="weekly_quiz_answer")
+                                    related_name="wq_question_id")
     answer = models.CharField(max_length=160)
+    response = models.CharField(max_length=160)
 
     def __unicode__(self):
         return self.answer
