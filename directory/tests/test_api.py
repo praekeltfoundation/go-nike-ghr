2b68c2a147a72ae3d41e63c038f087a00ebdf6c9
Mark Gituma <gitumarkk@gmail.com>
2013-07-05 18:31:49 +0200

    Added extra tests to API

--- directory/tests/test_api.py
+++ directory/tests/test_api.py
@@ -29,7 +29,21 @@ class TestCategoryApi(TestCase):
         self.assertIn("SC1B", json_item["directory"]["C1"])
 
         self.assertDictEqual(json_item["directory"]["C1"]["SC1A"],
-                             {"content_3": "C1Ac", "content_2": "C1Ab", "content_1": "C1Aa", })
+                             {"content_3": "C1Ac",
+                             "content_2": "C1Ab",
+                             "content_1": "C1Aa", })
 
         self.assertDictEqual(json_item["directory"]["C1"]["SC1B"],
-                             {"content_3": "C1Ba", "content_2": "C1Ba", "content_1": "C1Ba", })
+                             {"content_3": "C1Ba",
+                             "content_2": "C1Ba",
+                             "content_1": "C1Ba", })
+
+        self.assertDictEqual(json_item["directory"]["C2"]["SC2B"],
+                             {"content_3": "C2Bc",
+                             "content_2": "C2Bb",
+                             "content_1": "C2Ba", })
+
+        self.assertDictEqual(json_item["directory"]["C2"]["SC2A"],
+                             {"content_3": "C2Ac",
+                             "content_2": "C2Ab",
+                             "content_1": "C2Aa", })
