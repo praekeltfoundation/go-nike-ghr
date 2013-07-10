1213c6a7f3f32c5db72de6410303d7f07a51908e
Mark Gituma <gitumarkk@gmail.com>
2013-07-11 01:56:58 +0200

    Adding comments to API

--- user_interactions/api.py
+++ user_interactions/api.py
@@ -6,11 +6,13 @@ class UserInteractionResource(ModelResource):
     class Meta:
         resource_name = "userinteraction"
         allowed_methods = ['post']
-        list_allowed_methods = ['post']
+        list_allowed_methods = ['post']  # Tells API that POST is allowed for base URL
         include_resource_uri = False
-        # queryset = UserInteraction.objects.all()
 
     def post_list(self, request, **kwargs):
+        """
+        Gets posted data and stores it the the UserInteraction Table
+        """
         user_data = request.POST.dict()
         userinteraction = UserInteraction(msisdn=user_data['msisdn'],
                                           action=user_data['action'],
