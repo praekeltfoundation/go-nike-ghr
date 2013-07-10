119db2d9baf5a7542b54bac3fdd78c441fcab2b8
Mark Gituma <gitumarkk@gmail.com>
2013-07-11 00:40:11 +0200

    Created tests for the function

---
+++ user_interactions/tests/test_api.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+from user_interactions.models import UserInteraction
+
+
+class TestUserInteraction(TestCase):
+
+    def test_api_works(self):
+        url = reverse('api_dispatch_list',
+                      kwargs={'resource_name': 'userinteraction',
+                      'api_name': 'api'})
+        response = self.client.post(url, format="json", data={"msisdn": "msisdn",
+                                                "action": "action",
+                                                "transport": "transport"})
+
+        query = UserInteraction.objects.all()
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(query[0].msisdn, "msisdn")
+        self.assertEqual(query[0].action, "action")
+        self.assertEqual(query[0].transport, "transport")
