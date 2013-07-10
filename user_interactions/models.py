28d16feb5bbbdc64a2c8d7b7bbd76c96ac77b8f4
Mark Gituma <gitumarkk@gmail.com>
2013-07-10 23:51:47 +0200

    Creating the user interaction model

--- user_interactions/models.py
+++ user_interactions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class UserInteraction(models.Model):
+    # This class is for the Article model.
+    msisdn = models.CharField(max_length=20, blank=False)
+    action = models.CharField(max_length=200, blank=False)
+    transport = models.CharField(max_length=5, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      blank=False,
+                                      editable=False)
+
+    def __unicode__(self):
+        return self.msisdn
