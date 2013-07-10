e09182e24c86e81cf9785338939a3e0e167a2b99
Mark Gituma <gitumarkk@gmail.com>
2013-07-10 18:04:10 +0200

    Did unittests for the opinions

--- opinions/tests/test_api.py
+++ opinions/tests/test_api.py
@@ -8,13 +8,24 @@ class TestOpinion(TestCase):
 
     def test_api_works(self):
         url = reverse('api_dispatch_list',
-                      kwargs={'resource_name': 'opinion',
+                      kwargs={'resource_name': 'opinions/sms',
                       'api_name': 'api'})
 
         response = self.client.get(url)
         json_item = json.loads(response.content)
         self.assertEqual(response.request["PATH_INFO"],
-                         "/api/opinion/")
+                         "/api/opinions/sms/")
         self.assertEqual(response["Content-Type"], "application/json")
         self.assertIn("opinions", json_item)
         self.assertEqual(json_item["opinions"]["opinion_1"], "A")
+
+
+class TestOpinionPolls(TestCase):
+    fixtures = ['test/test_opinion_poll.json']
+
+    def test_opinion_polls_url(self):
+        url = reverse('api_dispatch_list',
+                      kwargs={'resource_name': 'opinions/view',
+                      'api_name': 'api'})
+        response = self.client.get(url)
+        print response
