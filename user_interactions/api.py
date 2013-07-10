3e31069b1f103a25f75527b6c979cab27ec88348
Mark Gituma <gitumarkk@gmail.com>
2013-07-11 00:39:58 +0200

    Function that adds data to database

--- user_interactions/api.py
+++ user_interactions/api.py
@@ -4,6 +4,15 @@ from tastypie.resources import ModelResource
 
 class UserInteractionResource(ModelResource):
     class Meta:
-        resource_name = UserInteraction
-        allowed_methods = ['get']
+        resource_name = "userinteraction"
+        allowed_methods = ['post']
+        list_allowed_methods = ['post']
         include_resource_uri = False
+        # queryset = UserInteraction.objects.all()
+
+    def post_list(self, request, **kwargs):
+        user_data = request.POST.dict()
+        userinteraction = UserInteraction(msisdn=user_data['msisdn'],
+                                          action=user_data['action'],
+                                          transport=user_data['transport'])
+        userinteraction.save()
