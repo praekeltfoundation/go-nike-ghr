13c57b9b6846edc4602e41ad36f7afe5bce3db17
Mark Gituma <gitumarkk@gmail.com>
2013-07-10 23:21:28 +0200

    Created unittests for API

--- opinions/tests/test_api.py
+++ opinions/tests/test_api.py
@@ -13,8 +13,6 @@ class TestOpinion(TestCase):
 
         response = self.client.get(url)
         json_item = json.loads(response.content)
-        self.assertEqual(response.request["PATH_INFO"],
-                         "/api/opinions/sms/")
         self.assertEqual(response["Content-Type"], "application/json")
         self.assertIn("opinions", json_item)
         self.assertEqual(json_item["opinions"]["opinion_1"], "A")
@@ -28,4 +26,30 @@ class TestOpinionPolls(TestCase):
                       kwargs={'resource_name': 'opinions/view',
                       'api_name': 'api'})
         response = self.client.get(url)
-        print response
+        json_item = json.loads(response.content)
+        self.assertEqual(response["Content-Type"], "application/json")
+        self.assertIn("opinions", json_item)
+        self.assertEqual(sorted(['opinion_view_1', 'opinion_view_2']),
+                         sorted([k for k in json_item["opinions"]]))
+
+        self.assertEqual(sorted(['start', 'views']),
+                         sorted([k for k in json_item["opinions"]['opinion_view_1']]))
+
+        self.assertEqual('o_1', json_item["opinions"]['opinion_view_1']["start"])
+        self.assertEqual('o_2', json_item["opinions"]['opinion_view_2']["start"])
+
+        self.assertEqual(sorted(['o_2', 'o_3']),
+                         sorted([k for k in json_item["opinions"]['opinion_view_2']["views"]]))
+
+        self.assertEqual(sorted(['opinions', 'choices']),
+                         sorted([k for k in json_item["opinions"]['opinion_view_2']["views"]["o_2"]]))
+
+        self.assertEqual('O2A', json_item["opinions"]['opinion_view_2']["views"]['o_2']['opinions'])
+
+        self.assertEqual(sorted([['o_3', 'C2Aa'], ['o_3', 'C2Ab']]),
+                         sorted([k for k in json_item["opinions"]['opinion_view_2']["views"]["o_2"]['choices']]))
+
+        self.assertEqual('O2B', json_item["opinions"]['opinion_view_2']["views"]['o_3']['opinions'])
+
+        self.assertEqual(sorted([['main_menu', 'C2Ba'], ['main_menu', 'C2Bb'], ['main_menu', 'C2Bc']]),
+                         sorted([k for k in json_item["opinions"]['opinion_view_2']["views"]["o_3"]['choices']]))
