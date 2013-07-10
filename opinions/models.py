ee25097d04101c6e47094831753e6fc5a3e12074
Mark Gituma <gitumarkk@gmail.com>
2013-07-10 18:02:47 +0200

    Updated opinions model to account for new structure

--- opinions/models.py
+++ opinions/models.py
@@ -17,3 +17,42 @@ class Opinion(models.Model):
 
     class Meta:
         verbose_name_plural = "User Opinions"
+
+
+class OpinionPollId(models.Model):
+    name = models.CharField(max_length=50, blank=False,
+                            verbose_name="Name of Opinion")
+    active = models.BooleanField()
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Opinion Poll Id"
+
+
+class OpinionPollOpinion(models.Model):
+    opinionpollid = models.ForeignKey('OpinionPollId',
+                                      related_name='opinionpollid',
+                                      verbose_name='Opinion Poll Opinion')
+    opinion = models.CharField(max_length=160, blank=False,
+                               verbose_name="Opinion")
+
+    def __unicode__(self):
+        return self.opinion
+
+    class Meta:
+        verbose_name_plural = "User Opinion Poll"
+
+
+class OpinionPollChoices(models.Model):
+    opinionpollopinion = models.ForeignKey('OpinionPollOpinion',
+                                           related_name='opinionpollopinion',
+                                           verbose_name='Opinion Poll Choices')
+    choices = models.CharField(max_length=160)
+
+    def __unicode__(self):
+        return self.choices
+
+    class Meta:
+        verbose_name_plural = "Opinion Poll Choices"
