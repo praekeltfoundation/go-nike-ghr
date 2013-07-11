49b2eb6893d8d8476f5efcdc2eb2eaeb4b58d3a3
Simon de Haan <simon@praekeltfoundation.org>
2013-07-11 15:00:14 +0200

    prepping travis

--- ghr/settings.py
+++ ghr/settings.py
@@ -26,10 +26,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ghr',
-        'USER': 'praekelt',
-        'PASSWORD': 'praekelt',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
