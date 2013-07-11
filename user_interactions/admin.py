6254fe652b1886519203d838c6e738e2bd5c76ba
Mark Gituma <gitumarkk@gmail.com>
2013-07-11 02:09:16 +0200

    Added export as CSV to admin

--- user_interactions/admin.py
+++ user_interactions/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from models import UserInteraction
+from actions import export_as_csv_action
 
 
 class UserInteractionAdmin(admin.ModelAdmin):
     list_display = ["msisdn", "transport", "created_at"]
 
 admin.site.register(UserInteraction, UserInteractionAdmin)
+admin.site.add_action(export_as_csv_action(), "export csv")
