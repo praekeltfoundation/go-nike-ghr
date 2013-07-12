2be3082a965c6c4b6eb6bfb21cb30e683781d9d0
Mike Jones <mike@westerncapelabs.com>
2013-07-12 17:44:41 +0200

    Removing extra test

--- directory/tests/test_api.py
+++ directory/tests/test_api.py
@@ -6,15 +6,6 @@ import json
 class TestCategoryApi(TestCase):
     fixtures = ['test_directory.json']
 
-    def test_correct_url(self):
-        url = reverse('api_dispatch_list',
-                      kwargs={'resource_name': 'category',
-                      'api_name': 'api'})
-        response = self.client.get(url)
-        self.assertEqual(response.request["PATH_INFO"], "/api/category/")
-        self.assertEqual("application/json", response["Content-Type"])
-        self.assertEqual(response.status_code, 200)
-
     def test_api_response(self):
         url = reverse('api_dispatch_list',
                       kwargs={'resource_name': 'category',
