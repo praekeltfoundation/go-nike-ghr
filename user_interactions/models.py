703a9eaf930b58ccbadfbc2f0ce2bdacb484b17e
Mark Gituma <gitumarkk@gmail.com>
2013-07-17 13:01:32 +0200

    Adding feature, value, key fields to model

--- user_interactions/models.py
+++ user_interactions/models.py
@@ -4,7 +4,9 @@ from django.db import models
 class UserInteraction(models.Model):
     # This class is for the User Interaction Model
     msisdn = models.CharField(max_length=20, blank=False)
-    action = models.CharField(max_length=200, blank=False)
+    feature = models.CharField(max_length=30, blank=False)
+    key = models.CharField(max_length=100, blank=False)
+    value = models.CharField(max_length=200, blank=False)
     transport = models.CharField(max_length=5, blank=False)
     created_at = models.DateTimeField(auto_now_add=True,
                                       blank=False,
