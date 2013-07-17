c7cc8f339c258c5589e04bcbeba525e2357223d3
Mike Jones <mike@westerncapelabs.com>
2013-07-17 19:20:48 +0200

    Updated to include all fields on admin view

--- user_interactions/admin.py
+++ user_interactions/admin.py
@@ -4,7 +4,8 @@ from actions import export_as_csv_action
 
 
 class UserInteractionAdmin(admin.ModelAdmin):
-    list_display = ["msisdn", "feature", "transport", "created_at"]
+    list_display = ["msisdn", "feature", "key", "value",
+                    "transport", "created_at"]
     actions = [export_as_csv_action("Export selected objects as CSV file",
                                     fields=["msisdn", "feature", "key", "value",
                                             "transport", "created_at"])]
