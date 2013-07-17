9240bf28376e3db78032e71f3224c7ba8782342e
Mark Gituma <gitumarkk@gmail.com>
2013-07-17 17:47:08 +0200

    Added field names to admin

--- user_interactions/admin.py
+++ user_interactions/admin.py
@@ -4,9 +4,9 @@ from actions import export_as_csv_action
 
 
 class UserInteractionAdmin(admin.ModelAdmin):
-    list_display = ["msisdn", "transport", "created_at"]
-    actions = [export_as_csv_action("CSV Export",
-                                    fields=["msisdn", "action",
+    list_display = ["msisdn", "feature", "transport", "created_at"]
+    actions = [export_as_csv_action("Export selected objects as CSV file",
+                                    fields=["msisdn", "feature", "key", "value",
                                             "transport", "created_at"])]
 
 admin.site.register(UserInteraction, UserInteractionAdmin)
