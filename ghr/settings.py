e5713e3cacb3822977bb03d8ba39465b7768613d
Simon de Haan <simon@praekeltfoundation.org>
2013-07-18 12:22:55 +0200

    fiddling with db settings

--- ghr/settings.py
+++ ghr/settings.py
@@ -27,9 +27,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ghr',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
