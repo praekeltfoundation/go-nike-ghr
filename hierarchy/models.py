85d123008b8524b2b61cbf70943289f8224af3bd
gitumarkk <gitumarkk@gmail.com>
2013-11-01 14:32:45 +0200

    added Province, District, Sector models

--- hierarchy/models.py
+++ hierarchy/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Province(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name=u'Name of Province')
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "1. Province"
+
+
+class District(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name=u'Name of District')
+    district_province = models.ForeignKey(Province,
+                                          verbose_name=u'Province')
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "2. District"
+
+
+
+class Sector(models.Model):
+    name = models.CharField(max_length=50, verbose_name=u'Name of Zone')
+    sector_district = models.ForeignKey(District,
+                                        verbose_name=u'District')
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "3. Sector"
