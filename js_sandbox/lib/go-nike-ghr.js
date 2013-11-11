456ad5b816c65ac55b9908d09044f5d87f6c7dec
Mike Jones <mike@westerncapelabs.com>
2013-11-11 10:36:38 +0200

    Fixed missing promise for counter

--- js_sandbox/lib/go-nike-ghr.js
+++ js_sandbox/lib/go-nike-ghr.js
@@ -527,7 +527,11 @@ function GoNikeGHR() {
                         if (result.contact["extras-ghr_last_active_week"] !== undefined){
                             if (new Date(wc) > new Date(result.contact["extras-ghr_last_active_week"])){
                                 update_req = true;
-                                return self.increment_and_fire_direct("ghr_ussd_total_users_"+wc);
+                                var piafd = self.increment_and_fire_direct("ghr_ussd_total_users_"+wc);
+                                piafd.add_callback(function(result) {
+                                    return true;
+                                });
+                                return piafd;
                             }
                         } else { // for contacts somehow missing attribute
                             update_req = true;
