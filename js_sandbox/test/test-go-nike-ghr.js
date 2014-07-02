631890dd9dfddf8164f79351991d736774cd399b
retrorabbitWD <wdawson@retrorabbit.co.za>
2014-07-02 13:31:15 +0200

    change the navigation options and edit tests

--- js_sandbox/test/test-go-nike-ghr.js
+++ js_sandbox/test/test-go-nike-ghr.js
@@ -784,7 +784,7 @@ describe("When using the USSD line", function() {
                 next_state: "articles",
                 response: (
                     "^Lorem ipsum dolor sit amet, consectetur adipiscing elit.[^]" +
-                     "1. Prev, 2. Next, 3. Main menu$"
+                     "2. Next$"
                 )
             });
             p.then(function() {
@@ -802,7 +802,7 @@ describe("When using the USSD line", function() {
                 next_state: 'articles',
                 response: "^Proin a porta justo. Maecenas sem felis, sollicitudin vitae " +
                           "risus luctus, consectetur sollicitudin leo.[^]" +
-                          "1. Prev, 2. Next, 3. Main menu$"
+                          "1. Prev, 2. Next$"
             });
             p.then(done, done);
         });
@@ -819,7 +819,7 @@ describe("When using the USSD line", function() {
                 next_state: 'articles',
                 response: "^Donec tincidunt lobortis erat eget malesuada. Cras cursus " +
                           "accumsan eleifend. Morbi ullamcorper pretium sollicitudin.[^]" +
-                           "1. Prev, 2. Next, 3. Main menu$"
+                           "1. Prev, 2. Next$"
             });
             p.then(done, done);
         });
@@ -836,24 +836,7 @@ describe("When using the USSD line", function() {
                 next_state: 'articles',
                 response: "^Etiam tincidunt, sapien elementum pharetra dapibus, " +
                           "mi sem venenatis nulla, at interdum sapien augue eu elit.[^]" +
-                          "1. Prev, 2. Next, 3. Main menu$"
-            });
-            p.then(done, done);
-        });
-
-        it('should continue to page 1 after page 4', function(done) {
-            var p = tester.check_state({
-                user: {
-                    pages: {
-                        articles: 3
-                    },
-                    current_state: 'articles'
-                },
-                content: "2",
-                next_state: 'articles',
-                response: "^Lorem ipsum dolor sit amet, consectetur adipiscing elit.[^]" +
-                          "1. Prev, 2. Next, 3. Main menu$",
-                continue_session: true
+                          "1. Prev, 3. Main Menu$"
             });
             p.then(done, done);
         });
@@ -889,7 +872,7 @@ describe("When using the USSD line", function() {
                 next_state: "wwsd",
                 response: (
                     "^Shangazi ipsum dolor sit amet, consectetur adipiscing elit.[^]" +
-                    "1. Prev, 2. Next, 3. Main menu$"
+                    "2. Next$"
                 )
             });
             p.then(function() {
@@ -907,7 +890,7 @@ describe("When using the USSD line", function() {
                 next_state: 'wwsd',
                 response: "^Shangazi a porta justo. Maecenas sem felis, sollicitudin vitae " +
                           "risus luctus, consectetur sollicitudin leo.[^]" +
-                           "1. Prev, 2. Next, 3. Main menu$"
+                           "1. Prev, 2. Next$"
             });
             p.then(done, done);
         });
@@ -924,7 +907,7 @@ describe("When using the USSD line", function() {
                 next_state: 'wwsd',
                 response: "^Shangazi tincidunt lobortis erat eget malesuada. Cras cursus " +
                           "accumsan eleifend. Morbi ullamcorper pretium sollicitudin.[^]" +
-                           "1. Prev, 2. Next, 3. Main menu$"
+                           "1. Prev, 2. Next$"
             });
             p.then(done, done);
         });
@@ -941,27 +924,11 @@ describe("When using the USSD line", function() {
                 next_state: 'wwsd',
                 response: "^Shangazi tincidunt, sapien elementum pharetra dapibus, " +
                           "mi sem venenatis nulla, at interdum sapien augue eu elit.[^]" +
-                          "1. Prev, 2. Next, 3. Main menu$"
+                          "1. Prev, 3. Main Menu$"
             });
             p.then(done, done);
         });
 
-        it('should continue to page 1 after page 4 of Shangazi Opinions', function(done) {
-            var p = tester.check_state({
-                user: {
-                    pages: {
-                        wwsd: 3
-                    },
-                    current_state: 'wwsd'
-                },
-                content: "2",
-                next_state: 'wwsd',
-                response: "^Shangazi ipsum dolor sit amet, consectetur adipiscing elit.[^]" +
-                           "1. Prev, 2. Next, 3. Main menu$",
-                continue_session: true
-            });
-            p.then(done, done);
-        });
 
         it('should continue to main menu after Shangazi Opinions finish', function(done) {
             var p = tester.check_state({
@@ -1035,7 +1002,7 @@ describe("When using the USSD line", function() {
                 next_state: "opinions_popular",
                 response: (
                     "^This is opinion one[^]" +
-                    "2. Next, 3. Main Menu.$"
+                    "2. Next$"
                 )
             });
             p.then(function() {
@@ -1054,7 +1021,7 @@ describe("When using the USSD line", function() {
                 next_state: "opinions_popular",
                 response: (
                     "^This is opinion two[^]" +
-                    "1. Prev, 2. Next, 3. Main Menu.$"
+                    "1. Prev, 2. Next$"
                 )
             });
             p.then(done, done);
@@ -1073,7 +1040,7 @@ describe("When using the USSD line", function() {
                 next_state: "opinions_popular",
                 response: (
                     "^This is opinion three[^]" +
-                    "1. Prev, 2. Next, 3. Main Menu.$"
+                    "1. Prev, 2. Next$"
                 )
             });
             p.then(done, done);
@@ -1092,7 +1059,7 @@ describe("When using the USSD line", function() {
                 next_state: "opinions_popular",
                 response: (
                     "^This is opinion four[^]" +
-                    "1. Prev, 2. Next, 3. Main Menu.$"
+                    "1. Prev, 2. Next$"
                 )
             });
             p.then(done, done);
@@ -1111,7 +1078,7 @@ describe("When using the USSD line", function() {
                 next_state: "opinions_popular",
                 response: (
                     "^This is opinion five[^]" +
-                    "1. Prev, 3. Main Menu.$"
+                    "1. Prev, 3. Main Menu$"
                 )
             });
             p.then(done, done);
